1b4e6aa45299415a2186fd8a1d7d5cbab687730a
Trent Osterman <trentosterman@gmail.com>
2023-05-09 10:24:14 -0500

    created a pie chart and a bar chart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Chart from 'chart.js/auto';
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "./utils/Data";
+import PieChart from "../src/components/PieChart"
+import BarChart from '../src/components/BarChart';
+
+
+Chart.register(CategoryScale);
 
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +45,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>Using Chart.js in React</p>
+        <PieChart chartData={chartData} />
+        <BarChart chartData={chartData}></BarChart>
       </header>
     </div>
   );
